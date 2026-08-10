634ebfa4bf2f6aa5810e2bb47b0d2a3dc03accd0
dev17122007 <devkumar.surani138386@marwadiuniversity.ac.in>
2026-08-10 15:06:16 +0530

    Delete UNIT-1 directory

--- UNIT-1/HelloImport.java
+++
@@ -1,8 +0,0 @@
-import java.lang.*;
-class HelloImport
-{
-	public static void main(String args[])
-	{
-		System.out.println("Hello Import");
-	}
-}
--- UNIT-1/PatternThree.java
+++
@@ -1,9 +0,0 @@
-public class PatternThree
-{
-	public static void main(String args[])
-	{
-		System.out.println("     0     0");
-		System.out.println("        !     ");
-		System.out.println("      ----  ");
-	}
-}
